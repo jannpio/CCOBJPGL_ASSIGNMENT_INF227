e58c5c402d71198007a523bc76895b2422344a07
Julie Ann Pioquinto <129469411+jannpio@users.noreply.github.com>
2023-05-23 21:38:02 +0800

    Add files via upload

---
+++ Exercise 5/model/PizzaHut.java
@@ -0,0 +1,5 @@
+package model;
+
+public class PizzaHut extends Restaurant {
+
+}
---
+++ Exercise 5/model/Restaurant.java
@@ -0,0 +1,23 @@
+package model;
+
+class Restaurant {
+
+    private String Recommendation;
+    private String Rating;
+
+    public String getRecommendation() {
+        return this.Recommendation;
+    }
+
+    public void setRecommendation(String Recommendation) {
+        this.Recommendation = Recommendation;
+    }
+
+    public String getRating() {
+        return this.Rating;
+    }
+
+    public void setRating(String Rating) {
+        this.Rating = Rating;
+    }
+}
